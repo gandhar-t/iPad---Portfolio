b215cf5e9775136cf216da7e302d792a7b69b8d8
Gandhar <gandhar.a.tannu@gmail.com>
2016-02-06 01:46:47 -0800

    Initial Commit

---
+++ Portfolio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Portfolio
+//
+//  Created by Gandhar on 2016-02-06.
+//  Copyright © 2016 gandhartannu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Portfolio/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Portfolio
+//
+//  Created by Gandhar on 2016-02-06.
+//  Copyright © 2016 gandhartannu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
